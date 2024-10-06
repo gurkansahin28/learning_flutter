eb1659cafc412dc9893f5e313a230fd1c1b8e042
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-06 10:56:00 +0300

    Create clip_r_rect.dart

---
+++ asset/image/clip_r_rect.dart
@@ -0,0 +1,6 @@
+//--------------------------------------------------------------------------------------
+Center(child: ClipRRect(
+  borderRadius: BorderRadius.circular(40.0),
+  child: const Image(image: AssetImage('assets/images/mo.png')),
+  ),),
+//-------------------------------------------------------------------------------------
