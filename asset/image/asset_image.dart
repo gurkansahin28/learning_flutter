a3069bb0588cb844b712536361ee7825d87de447
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-06 10:53:23 +0300

    Update asset_image.dart

--- asset/image/asset_image.dart
+++ asset/image/asset_image.dart
@@ -1,1 +1,19 @@
 //---about AssetImage
+    return  Scaffold(
+        appBar: AppBar(title: const Text('images'),),
+        body: const Column(
+          children: [
+            Center(
+              child: Image(image: AssetImage('assets/images/mo.png'),),
+            ),
+            //---the other widgets
+          ],
+        ),
+    );
+
+/*
+flutter:
+  uses-material-design: true
+  assets:
+  - assets/images/mo.png
+*/
