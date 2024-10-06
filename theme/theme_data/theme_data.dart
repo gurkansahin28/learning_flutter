9d8ee8d5a99eddf5b1317513c45875ba129a1ab8
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-06 10:58:58 +0300

    Update theme_data.dart

--- theme/theme_data/theme_data.dart
+++ theme/theme_data/theme_data.dart
@@ -1,1 +1,25 @@
 //---abot the ThemeData widget
+//--------------------------------------------------------------------------------------
+    return MaterialApp(
+      //---ThemeData---------
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple, brightness: Brightness.dark),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+        ),
+      ),
+      //---------------------
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        appBar: AppBar(title: Text(_message),backgroundColor: Colors.purple,),
+        
+        //body: const Center(child: Text('leaning to fly from crawling...'),),
+        body: Center(
+          child: Container(color: Colors.grey, child: const Column(
+            children: [Row(children: [Text('data1'), Text('data2')],)],
+          ),),
+        ),
+      ),
+    );
+//---------------------------------------------------------------------------------------
