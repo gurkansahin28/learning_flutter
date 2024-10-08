1199ef437bbb4cc8a3f4f58ded4fe634feb2af33
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-08 19:36:36 +0300

    Create clip_r_rect.dart

---
+++ stylizing/clip_r_rect.dart
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------------------------------
+ClipRRect(
+  borderRadius: BorderRadius.circular(10.0),
+  child: Container(
+    height: 40,
+    color: Colors.amber[900],
+    child: const Center(child: Text('Rounded List Item'),),
+  ),
+),
+//-----------------------------------------------------------------------------------
